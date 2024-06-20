f20e3b8082a8412fc176f26f00df26b598856c53
dimaszdn <dimaszdn@mail.ru>
2024-06-20 22:53:52 +0200

    adding the menu to the application

--- inc/App.h
+++ inc/App.h
@@ -5,13 +5,18 @@
 #include <SFML/Graphics.hpp>
 #include <Map.h>
 #include <BFS.h>
+#include <Info.h>
+#include <Menu.h>
 
 class App
 {
 private:
     sf::RenderWindow mWindow;
+    sf::Event event{};
     sf::Color fontColor = {250,235,215};
 
+    int mMenuW{};
+    int mMenuH{};
     bool startBfsButtonIsPressed = false;
 
 public:
@@ -21,11 +26,17 @@ public:
 
     void setup(int windowW, int windowH, const std::string& caption)
     {
-        mWindow.create(sf::VideoMode(windowW, windowH), caption);
+        mWindow.create(sf::VideoMode(windowW, windowH), caption, sf::Style::Titlebar);
         mWindow.setFramerateLimit(60);
     }
 
-    void logic(BFS& bfs);
+    void setMenuSize(int menuW, int menuH)
+    {
+        mMenuW = menuW;
+        mMenuH = menuH;
+    }
+
+    void logic(BFS& bfs, Menu& menu);
 
     void run();
 };
