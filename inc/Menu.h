8ad2734cc34a4fa6b9bf898cbabc83b386e6e70e
dimaszdn <dimaszdn@mail.ru>
2024-06-23 23:10:38 +0200

    new installations + user input

--- inc/Menu.h
+++ inc/Menu.h
@@ -1,32 +1,47 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
-#include <Info.h>
+#include <WindowText.h>
+#include <Button.h>
 
 class Menu
 {
 private:
     sf::RenderWindow& mWindow;
-
     sf::Vector2i mMenuSize;
 
     int characterSizeTitle = 80;
     int characterSizeInfo = 60;
-
-    Info title;
-    Info bfsInfo;
+    WindowText title;
+    WindowText bfsInfo;
+    Button bfsButton;
+    Button exitButton;
 
 public:
     Menu(sf::Vector2i menuSize, sf::RenderWindow& window) : mMenuSize(menuSize), mWindow(window)
     {}
 
-    void setupTitle(const std::string& str);
+    void setTitle(const std::string& str);
 
-    void setupBfsInfo(const std::string& str);
+    void setBfsInfo(const std::string& str, const sf::Color& color);
+
+    void setButtonStartBfs();
+
+    void setButtonExit();
+
+    void inputUser(bool& startBfsButtonIsPressed, bool& exitButtonIsPressed)
+    {
+        if (bfsButton.isMouseOver(mWindow) && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+            startBfsButtonIsPressed = true;
+        if (exitButton.isMouseOver(mWindow) && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+            exitButtonIsPressed = true;
+    }
 
     void draw()
     {
         mWindow.draw(title.getText());
         mWindow.draw(bfsInfo.getText());
+        bfsButton.draw(mWindow);
+        exitButton.draw(mWindow);
     }
 };
