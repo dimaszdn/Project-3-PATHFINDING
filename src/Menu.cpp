3455c3cadd9e13659477e0d7c88395050b512675
dimaszdn <dimaszdn@mail.ru>
2024-06-23 23:10:47 +0200

    new installations + user input

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,14 +1,17 @@
 #include <Menu.h>
-
 #include <iostream>
 
-void Menu::setupTitle(const std::string &str)
+void Menu::setTitle(const std::string &str)
 {
     try
     {
-        title.setup(characterSizeTitle, sf::Color::Blue);
-        title.setPos({(float)(mWindow.getSize().x - characterSizeTitle - (mMenuSize.x / 2)), 10});
         title.setString(str);
+        title.setup(80, sf::Color::Blue);
+
+        float x = mWindow.getSize().x - mMenuSize.x;
+        float y = 30;
+        title.setPosition({x, y});
+        title.centerByWidth(mMenuSize.x);
     }
     catch (const std::exception& ex)
     {
@@ -16,16 +19,56 @@ void Menu::setupTitle(const std::string &str)
     }
 }
 
-void Menu::setupBfsInfo(const std::string &str)
+void Menu::setBfsInfo(const std::string &str, const sf::Color& color)
 {
     try
     {
-        bfsInfo.setup(50, sf::Color::Blue);
-        bfsInfo.setPos({(float)(mWindow.getSize().x - mMenuSize.x + 5), (float)(characterSizeInfo * 2)});
         bfsInfo.setString(str);
+        bfsInfo.setup(50, color);
+
+        float x = mWindow.getSize().x - mMenuSize.x + 10;
+        float y = bfsInfo.getText().getCharacterSize() * 3;
+        bfsInfo.setPosition({x, y});
     }
     catch (const std::exception& ex)
     {
         std::cout << ex.what() << "\n";
     }
 }
+
+void Menu::setButtonStartBfs()
+{
+    try
+    {
+        bfsButton.setup({300, 100}, sf::Color::White);
+        bfsButton.setText("Start BFS", 100 / 2, sf::Color::Blue);
+
+        float x = mWindow.getSize().x - mMenuSize.x;
+        float y = 540;
+        bfsButton.setPosition({x, y});
+        bfsButton.centerByWidth(mMenuSize.x);
+    }
+    catch (const std::exception& ex)
+    {
+        std::cout << ex.what() << "\n";
+    }
+}
+
+void Menu::setButtonExit()
+{
+    try
+    {
+        exitButton.setup({300, 100}, sf::Color::White);
+        exitButton.setText("Exit", 100 / 2, sf::Color::Blue);
+
+        float x = mWindow.getSize().x - mMenuSize.x;
+        float y = 840;
+        exitButton.setPosition({x, y});
+        exitButton.centerByWidth(mMenuSize.x);
+    }
+    catch (const std::exception& ex)
+    {
+        std::cout << ex.what() << "\n";
+    }
+}
+
